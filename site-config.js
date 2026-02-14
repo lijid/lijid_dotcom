03fbc68494d9e9562e3e42af24ba90841aec02c4
Deepak Narayanan <deepaknarayanan@Deepaks-MacBook-Air.local>
2026-02-13 22:32:14 -0600

    Configure Google Maps API key for live reviews

--- site-config.js
+++ site-config.js
@@ -1,5 +1,5 @@
 window.SITE_CONFIG = {
-  googleMapsApiKey: "",
+  googleMapsApiKey: "AIzaSyBTdNgqHxukFqW5YC-vBMJyBZGp3HozksI",
   googlePlaceId: "ChIJ9WYin_E39ocRjjv-SfNSlAY",
   maxLiveReviews: 4,
 };
